bdcc3262b917841a20f29b5748e9d3be010478f0
Oleksandr Klymenko <e12038131@student.tuwien.ac.at>
2026-04-02 13:50:04 +0200

    db dixit

--- js/database.js
+++ js/database.js
@@ -18,7 +18,7 @@ export const boardGames = [
 
         assets: {
             cover: "./assets/dixit/cover.jpg",
-            rules: "./assets/rules/dixit-rules.pdf",
+            rules: "./assets/dixit/rules.pdf",
             logo: "./assets/dixit/logo.png",
             cardsPdf: "assets/dixit/dixit-cards.pdf",
             gallery: ["./assets/dixit/cover.jpg", "./assets/dixit/gal2.png", "./assets/dixit/gal1.png"] 
@@ -30,18 +30,74 @@ export const boardGames = [
             {
                 id: "dixit-quest",
                 title: "Dixit: Quest",
-                description: "84 нові карти з приголомшливими ілюстраціями від Марі Кардуа.",
+                description: "Відкрийте для себе казкову подорож у світ дитинства та безтурботності. <br> 84 нові карти з приголомшливими ілюстраціями від Марі Кардуа.",
                 owned: true,
-                cardsPdf: "./assets/rules/dixit-cards.pdf",
-                cover: "./assets/dixit/cover.jpg" 
+                cardsPdf: "./assets/dixit/quest.pdf",
+                cover: "./assets/dixit/quest.png" 
+            },
+            {
+                id: "dixit-journey",
+                title: "Dixit: Journey",
+                description: "Пориньте у таємничу та чарівну мандрівку, повну несподіваних відкриттів. <br> 84 нові карти з приголомшливими ілюстраціями від Ксав'є Коллета.",
+                owned: false,
+                cardsPdf: "./assets/dixit/journey.pdf",
+                cover: "./assets/dixit/journey.png" 
+            },
+            {
+                id: "dixit-origins",
+                title: "Dixit: Origins",
+                description: "Поверніться до витоків фантастичних світів та міфічних створінь. <br> 84 нові карти з приголомшливими ілюстраціями від Клемана Лефевра.",
+                owned: false,
+                cardsPdf: "./assets/dixit/origins.pdf",
+                cover: "./assets/dixit/origins.png" 
+            },
+            {
+                id: "dixit-daydreams",
+                title: "Dixit: Daydreams",
+                description: "Відчуйте емоції снів наяву. Сюрреалістичний світ, де фантазія межує з реальністю. <br> 84 нові карти з приголомшливими ілюстраціями від Франка Діона.",
+                owned: false,
+                cardsPdf: "./assets/dixit/daydreams.pdf",
+                cover: "./assets/dixit/daydreams.png" 
+            },
+            {
+                id: "dixit-memories",
+                title: "Dixit: Memories",
+                description: "Знайдіть яскраві спогади з дитинства в екзотичних та живих пейзажах. <br> 84 нові карти з приголомшливими ілюстраціями від Карін Хіндер та Жерома Пелісьє.",
+                owned: false,
+                cardsPdf: "./assets/dixit/memories.pdf",
+                cover: "./assets/dixit/memories.png" 
+            },
+            {
+                id: "dixit-revelations",
+                title: "Dixit: Revelations",
+                description: "Насолоджуйтесь магічними та витонченими ілюстраціями, натхненними античною міфологією. <br> 84 нові карти з приголомшливими ілюстраціями від Марини Кудре.",
+                owned: true,
+                cardsPdf: "./assets/dixit/revelations.pdf",
+                cover: "./assets/dixit/revelations.png" 
             },
             {
                 id: "dixit-harmonies",
                 title: "Dixit: Harmonies",
-                description: "Чудове доповнення з акцентом на природні та казкові мотиви.",
+                description: "Відкрийте, як контраст стає джерелом гармонії у цьому дивному та прекрасному світі. <br> 84 нові карти з приголомшливими ілюстраціями від Поля Ешегоєна.",
+                owned: true,
+                cardsPdf: "./assets/dixit/harmonies.pdf",
+                cover: "./assets/dixit/harmonies.png"
+            },
+            {
+                id: "dixit-anniversary",
+                title: "Dixit: Anniversary",
+                description: "Святкуємо 10-річчя Dixit казками та легендами з усього світу. <br> 84 нові карти з приголомшливими ілюстраціями від усіх ілюстраторів серії.",
                 owned: false,
-                cardsPdf: "./assets/rules/dixit-cards.pdf",
-                cover: "./assets/dixit/cover.jpg"
+                cardsPdf: "./assets/dixit/anniversary.pdf",
+                cover: "./assets/dixit/anniversary.png" 
+            },
+            {
+                id: "dixit-mirrors",
+                title: "Dixit: Mirrors",
+                description: "Дозвольте собі здивуватися казковим відображенням нашого повсякденного життя. <br> 84 нові карти з приголомшливими ілюстраціями від Себастьяна Теллескі.",
+                owned: false,
+                cardsPdf: "./assets/dixit/mirrors.pdf",
+                cover: "./assets/dixit/mirrors.png" 
             }
         ]
     },
